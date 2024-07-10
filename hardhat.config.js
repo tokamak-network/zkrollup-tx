86dceb63c0213588daf2b609f79ef3ea9b644942
jdhyun09 <jdhyun09@naver.com>
2024-07-10 14:37:29 +0900

    feat: add single tx tutorial

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,19 +18,22 @@ module.exports = {
     inputBasePath: "./circuits",
     ptau: "https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau",
     circuits: [
+      // {
+      //   name: "division",
+      //   // No protocol, so it defaults to groth16
+      // },
+      // {
+      //   name: "simple-polynomial",
+      //   // Generate PLONK
+      //   protocol: "plonk",
+      // },
+      // {
+      //   name: "hash",
+      //   // Explicitly generate groth16
+      //   protocol: "groth16",
+      // },
       {
-        name: "division",
-        // No protocol, so it defaults to groth16
-      },
-      {
-        name: "simple-polynomial",
-        // Generate PLONK
-        protocol: "plonk",
-      },
-      {
-        name: "hash",
-        // Explicitly generate groth16
-        protocol: "groth16",
+        name: "single_tx/singleTx",
       },
     ],
   },
